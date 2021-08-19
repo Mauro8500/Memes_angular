875f90cca425b8c8bdbdc596942f7b5e9cabc2c6
Mauricio Illanes <mauricioillanes17@gmail.com>
2021-08-19 02:16:54 -0400

    main

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Meme } from './meme'
+//import { Observable, throwError } from 'rxjs';
+//import { catchError, retry } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'memes-rest';
+  public mms: Meme[] = [];
+
+  constructor(private http: HttpClient){
+
+  }
+
+
+  onSave(): void {
+    console.log("Antes de invocar al servidor REST en backend");
+    this.http.get<any>("https://api.imgflip.com/get_memes").subscribe(response => {
+      this.mms = response.data.memes;
+
+    });
+    console.log("Despues de invocar al servidor REST en backend");
+
+  }
 }
